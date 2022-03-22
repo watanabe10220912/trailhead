f60a1faf3641936f02fadd8eeb70890b131495af
gen.watanabe <gen.watanabe@accenture.com>
2022-03-22 10:54:39 +0900

    Handle Server Errors

--- develop/force-app/main/default/lwc/contactList/contactList.js
+++ develop/force-app/main/default/lwc/contactList/contactList.js
@@ -1,4 +1,5 @@
 import { LightningElement, wire } from 'lwc';
+import { reduceErrors } from 'c/ldsUtils';
 import FIRST_NAME_FIELD from '@salesforce/schema/Contact.FirstName';
 import LAST_NAME_FIELD from '@salesforce/schema/Contact.LastName';
 import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
@@ -13,4 +14,7 @@ export default class ContactList extends LightningElement {
     columns = COLUMNS;
     @wire(getContacts)
     contacts;
+    get errors() {
+        return (this.contacts.error) ? reduceErrors(this.contacts.error) : [];
+    }
 }
